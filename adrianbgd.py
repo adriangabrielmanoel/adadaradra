7fcc5fc290dc9519dca73881cd997de25bd1bfc2
adriangabrielmanoel <159543227+adriangabrielmanoel@users.noreply.github.com>
2025-02-14 12:18:00 -0300

    Add files via upload

---
+++ adrianbgd.py
@@ -0,0 +1,20 @@
+print('adrianbgd\n')
+
+
+print('1.cadastrar restaurante')
+print('2.cadastrar restaurante')
+print('3.ativar restaurante')
+print('4.sair')
+
+
+opcao_escolhida = int(input('escolha uma opcao:')) 
+ print(f'voce escolheu a opcao {opcao_escolhida}')
+ if opcao_escolhida == 1:
+   print('cadastrar restaurante')
+elif opcao_escolhida == 2:
+print ('cadastrar restaurante')
+elif opcao_escolhida == 3 :
+('ativar restaurante')
+   print ('ativar restaurante')
+else: 
+   print('encerrando o programa')
